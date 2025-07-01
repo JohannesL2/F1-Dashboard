72c5e6b1cdf79f71f8c2a6dcfb104949b146cde8
johannesL2 <johannes.lindgren2002@gmail.com>
2025-07-01 14:57:36 +0200

    Added skeleton when loading, added team colour

--- src/pages/DriverDashboard.jsx
+++ src/pages/DriverDashboard.jsx
@@ -5,20 +5,95 @@ import { Link } from 'react-router-dom'
 
 const DriverDashboard = () => {
     const [carData, setCarData] = useState([]);
+    const [loading, setLoading] = useState(true)
 
   useEffect(() => {
   const fetchData = async () => {
     try {
     const res = await axios.get('https://api.openf1.org/v1/drivers');
     setCarData(res.data.slice(0, 20));
-    console.log(res.data)
+    setLoading(false)
   } catch (error) {
     console.error('Error fetching f1 data', error);
+    setLoading(false)
   }
 }
 
 fetchData();
 }, []);
+
+
+useEffect(() => {
+  const timer = setTimeout(() => setLoading(false), 5000);
+  return () => clearTimeout(timer);
+}, []);
+
+//skeleton
+function SkeletonLoader() {
+  return (
+    
+<div role="status" class="fixed inset-0 p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded-sm shadow-sm animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 mt-20">
+    <div class="flex items-center justify-between">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+        <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+        <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+        <div class="flex items-center justify-between pt-4">
+        <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+    </div>
+    <span class="sr-only">Loading...</span>
+</div>
+
+  )
+}
 
 
   return (
@@ -30,6 +105,10 @@ fetchData();
       > <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"/></svg>
       Go back</Link>
 
+    
+    {loading ? (
+      <SkeletonLoader/>
+    ) : (
     <table className="table-auto w-full">
       <thead>
         <tr className="bg-gray-100">
@@ -37,6 +116,7 @@ fetchData();
           <th>Driver number</th>
           <th>Name</th>
           <th>Team</th>
+          <th>Team Colour</th>
           <th>Country</th>
         </tr>
       </thead>
@@ -47,11 +127,13 @@ fetchData();
       <td>{item.driver_number}</td>
       <td>{item.full_name}</td>
       <td>{item.team_name}</td>
+      <td style={{backgroundColor: `#${item.team_colour}`}}></td>
       <td>{item.country_code}</td>
       </tr>
     ))}
     </tbody>
     </table>
+    )}
     </div>
   )
 }
