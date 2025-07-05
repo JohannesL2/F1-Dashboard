f161bb2a3fa5e1293c948a453801ff41f61b9268
johannesL2 <johannes.lindgren2002@gmail.com>
2025-07-05 14:38:53 +0200

    Showing menu buttons doesn't work by greying them out, this gives the user a better experience

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -25,20 +25,20 @@ const HomePage = () => {
       Radio Dashboard</Link>
       
       <Link 
-      to='/DriverDashboard'
-      className='bg-red-600 text-white whitespace-normal break-words rounded-lg p-4 shadow-lg py-10 text-center text-3xl hover:text-black border-5 transition-all duration-300 ease-in-out'
+      to=''
+      className='bg-gray-400 text-gray-700 cursor-not-allowed text-white whitespace-normal break-words rounded-lg p-4 shadow-lg py-10 text-center text-3xl hover:text-black border-5 transition-all duration-300 ease-in-out'
       ><FaCarSide className='inline mr-2'/>
       Car Dashboard</Link>
 
       <Link 
-      to='/DriverDashboard'
-      className='bg-red-600 text-white whitespace-normal break-words rounded-lg p-4 shadow-lg py-10 text-center text-3xl hover:text-black border-5 transition-all duration-300 ease-in-out'
+      to=''
+      className='bg-gray-400 text-gray-700 cursor-not-allowed text-white whitespace-normal break-words rounded-lg p-4 shadow-lg py-10 text-center text-3xl hover:text-black border-5 transition-all duration-300 ease-in-out'
       ><GoLocation className='inline mr-2'/>
       Location Dashboard</Link>
 
       <Link 
-      to='/DriverDashboard'
-      className='bg-red-600 text-white whitespace-normal break-words rounded-lg p-4 shadow-lg py-10 text-center text-3xl hover:text-black border-5 transition-all duration-300 ease-in-out'
+      to=''
+      className='bg-gray-400 text-gray-700 cursor-not-allowed text-white whitespace-normal break-words rounded-lg p-4 shadow-lg py-10 text-center text-3xl hover:text-black border-5 transition-all duration-300 ease-in-out'
       ><GiStopwatch className='inline mr-2'/>
       Laps Dashboard</Link>
     </div>
