4f3e732f96d75bea94517b9a73c7148a3707724c
johannesL2 <johannes.lindgren2002@gmail.com>
2025-09-23 15:49:45 +0200

    Changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { FaGithub } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div className='bg-black text-white p-4 shadow-md flex gap-4 items-center'>
-    <h1 className="text-xl font-extrabold tracking-widest text-white uppercase">
+    <div className='w-full bg-black text-white p-4 shadow-md flex items-center justify-center gap-6 hover:bg-neutral-900 transition-colors'>
+    <h1 className="text-xs sm:text-sm font-extrabold tracking-widest uppercase select-none">
       Created by Johannesl2 using OpenF1 API
     </h1>
-    <a href="https://github.com/JohannesL2/F1-Dashboard" target="_blank">
+    <a href="https://github.com/JohannesL2/F1-Dashboard" target="_blank" rel="noopener noreferrer">
     <FaGithub size={32} className='hover:text-stone-300'/>
     </a>
     </div>
